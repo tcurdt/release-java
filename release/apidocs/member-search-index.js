8692e181036d6d0b3cdf5a64f0834fe249d4f4af
tcurdt <tcurdt@users.noreply.github.com>
2021-12-15 12:34:10 +0000

    deploy: 5f1e1ba64929a75edbc516b0d1187051f406394d

---
+++ release/apidocs/member-search-index.js
@@ -0,0 +1,1 @@
+memberSearchIndex = [{"p":"org.vafer","c":"Main","l":"Main()","u":"%3Cinit%3E()"},{"p":"org.vafer","c":"Main","l":"main(String[])","u":"main(java.lang.String[])"}];updateSearchResults();
